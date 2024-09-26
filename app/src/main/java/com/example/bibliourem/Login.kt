c5b3f6a01b55680d1af6dbb0a5bf90f35afa16cd
werllon <paulowerllon@hotmai.com>
2024-09-25 21:20:51 -0300

    paginas ok

--- app/src/main/java/com/example/bibliourem/Login.kt
+++ app/src/main/java/com/example/bibliourem/Login.kt
@@ -1,20 +1,37 @@
 package com.example.bibliourem
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Login : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() // Certifique-se de que esta função está implementada
         setContentView(R.layout.activity_login)
+
+        // Ajusta as margens de acordo com as barras do sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Intent para abrir a tela de cadastro
+        val btnCadastro = findViewById<Button>(R.id.btnCadastro)
+        btnCadastro.setOnClickListener {
+            // Use a classe CadastroActivity para abrir a nova tela
+            val intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    class CadastroActivity {
+
     }
 }
--- app/src/main/java/com/example/bibliourem/Perfil.kt
+++ app/src/main/java/com/example/bibliourem/Perfil.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class perfl : AppCompatActivity() {
+class Perfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_perfl)
+        setContentView(R.layout.activity_perfil)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
