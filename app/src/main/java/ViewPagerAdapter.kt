a011a312460db59ac0457cdaf27ba5521513c12f
WerlonSantos <paulowerlon@hotmail.com>
2024-10-10 17:24:21 -0300

    paginas

--- app/src/main/java/ViewPagerAdapter.kt
+++ app/src/main/java/ViewPagerAdapter.kt
@@ -1,6 +1,7 @@
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.bibliourem.InicioFragment
 
 class ViewPagerAdapter(fragmentActivity: FragmentActivity) :
     FragmentStateAdapter(fragmentActivity) {
