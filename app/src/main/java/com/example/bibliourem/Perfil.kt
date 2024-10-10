42eea709252f579d42ab36e24579566e7b8f76c4
werllon <paulowerllon@hotmai.com>
2024-10-09 21:12:05 -0300

    camera ok

--- app/src/main/java/com/example/bibliourem/Perfil.kt
+++ app/src/main/java/com/example/bibliourem/Perfil.kt
@@ -1,20 +1,30 @@
 package com.example.bibliourem
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.MediaStore
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Perfil : AppCompatActivity() {
+
+    private val REQUEST_IMAGE_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_perfil)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Acessa o botão pelo ID e define o clique para abrir a câmera
+        val btnFoto: Button = findViewById(R.id.btnfoto)
+        btnFoto.setOnClickListener {
+            abrirCamera()
+        }
+    }
+
+    private fun abrirCamera() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
         }
     }
 }
